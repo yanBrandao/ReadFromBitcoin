8179dd3eacb96a701f511ffd4b4a68b4a94f4b84
Yan Tapajós <yan.diniz@itbam.org.br>
2018-10-19 09:08:34 -0400

    Tela de Home Adicionada para realizar a leitura do Ticker do Mercado Bitcoin

--- ReadFromBitCoin/Telas/Frm_Home.Designer.cs
+++ ReadFromBitCoin/Telas/Frm_Home.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReadFromBitCoin
 {
-    partial class Form1
+    partial class Frm_Home
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMercadoBitCoin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMercadoBitCoin
+            // 
+            this.btnMercadoBitCoin.Location = new System.Drawing.Point(26, 364);
+            this.btnMercadoBitCoin.Name = "btnMercadoBitCoin";
+            this.btnMercadoBitCoin.Size = new System.Drawing.Size(86, 46);
+            this.btnMercadoBitCoin.TabIndex = 0;
+            this.btnMercadoBitCoin.Text = "Mercado BitCoin";
+            this.btnMercadoBitCoin.UseVisualStyleBackColor = true;
+            this.btnMercadoBitCoin.Click += new System.EventHandler(this.btnMercadoBitCoin_Click);
+            // 
+            // Frm_Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMercadoBitCoin);
+            this.Name = "Frm_Home";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMercadoBitCoin;
     }
 }
 
---
+++ ReadFromBitCoin/Telas/Frm_Home.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using ReadFromBitCoin.Códigos_Auxiliares;
+using System.IO;
+using System.Threading;
+
+namespace ReadFromBitCoin
+{
+    public partial class Frm_Home : Form
+    {
+
+        string requestMercadoBitcoin = "https://www.mercadobitcoin.net/api/";
+        bool runningTaskMercado = false;
+
+        public Frm_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMercadoBitCoin_Click(object sender, EventArgs e)
+        {
+            if (btnMercadoBitCoin.Text.Equals("Stop"))
+            {
+                btnMercadoBitCoin.Text = "Mercado Bitcoin";
+                runningTaskMercado = false;
+            }
+            else
+            {
+                btnMercadoBitCoin.Text = "Stop";
+                runningTaskMercado = true;
+                Task.Factory.StartNew(() => { ReadJSON(); });
+            }
+            Application.DoEvents();
+        }
+
+        private void ReadJSON()
+        {
+            while (runningTaskMercado)
+            {
+                Request(Enumeraveis.Moedas.BTC);
+                Thread.Sleep(800);
+            }
+        }
+        
+        public string Request(Enumeraveis.Moedas moeda)
+        {
+            Console.WriteLine("Request " + DateTime.Now.Minute + ":" + DateTime.Now.Second);
+            string totalURL = requestMercadoBitcoin + moeda + "/ticker/";
+
+            return this.HttpGet(totalURL);
+        }
+
+        public string HttpGet(string URI)
+        {
+            WebClient cliente = new WebClient();
+
+            cliente.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            Stream data = cliente.OpenRead(URI);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+
+            return s;
+        }
+
+
+    }
+}
