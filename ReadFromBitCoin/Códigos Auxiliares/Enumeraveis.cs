57f336863d3dd60d6f281ee9eed9ee1fe058a134
Yan Tapajós <yan.diniz@itbam.org.br>
2018-10-19 09:07:37 -0400

    Tipos de Moedas definidos.

---
+++ ReadFromBitCoin/Códigos Auxiliares/Enumeraveis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromBitCoin.Códigos_Auxiliares
+{
+    public class Enumeraveis
+    {
+        public enum Moedas
+        {
+            [Description("Bitcoin")]
+            BTC,
+            [Description("Litecoin")]
+            LTC,
+            [Description("BCash")]
+            BCH,
+            [Description("Ripple")]
+            XRP
+        }
+    }
+}
